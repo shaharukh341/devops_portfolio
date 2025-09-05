a488e2eaac30bd7adb7df573a9961b9f031349ff
SHARUKH AHMED SHAIK <sharukh.ahmed.04@gmail.com>
2025-09-05 20:42:56 +0530

    updated slider

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -126,7 +126,7 @@ const Work = () => {
               {projects.map((project, index) => {
                 return (
                   <SwiperSlide key={index} className="w-full">
-                    <div className="h-[0px] relative group flex justify-center items-center bg-pink-50/20">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-primary-50/20">
                       {/* overlay */}
                       <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                       {/* image */}
